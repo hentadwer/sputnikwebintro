c46987deacfc65feac67fee1987b767eb779fe13
hentadwer <sputnikweb@gmail.com>
2016-10-24 15:38:45 -0500

    validar con ajax
    validando los comamos con ajax

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,27 @@
 <?php
+// Guardar los datos recibidos en variables:
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+// Definir el correo de destino:
+$dest = "sputnikweb@gmail.com";
 
-$host = 'localhost';
-$usuario = 'root';
-$pass = 'root';
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $nombre <$email>\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$conn = mysql_connect($host, $usuario, $pass) or die ('Error conectando a la base de datos');
+// Aqui definimos el asunto y armamos el cuerpo del mensaje
+$asunto = "Contacto";
+$cuerpo = "Nombre: ".$nombre."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+$cuerpo .= "Telefono: ".$telefono."<br>";
+$cuerpo .= "Mensaje: ".$mensaje;
 
-$bdnombre = 'cf';
-mysql_select_db($bdnombre);
+// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
+if($nombre != '' && $email != '' && $telefono != '' && $mensaje != ''){
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
+}
+?>
